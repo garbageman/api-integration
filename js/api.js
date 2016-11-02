800a4ee33ecfecd91525f400fed2b026082a3dcc
garbageman <dnagle@umd.edu>
2016-11-02 13:24:15 -0400

    Finished thing

---
+++ js/api.js
@@ -0,0 +1,12 @@
+'use-strict';
+
+var pokeapi = 'http://pokeapi.co/api/v2/pokemon/';
+
+var toggleimage = function(element) {
+  var pokemonName = $('#pokemonname').val();
+  $.ajax({
+    url: pokeapi + pokemonName
+  }).done(function (response) {
+    document.getElementById('pokemonpic').src = response.sprites.front_default;
+  });
+};
